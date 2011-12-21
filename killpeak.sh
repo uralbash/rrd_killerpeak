80e5f26fa18cb53d9c20e7f5babb983687d73634
uralbash <spam@19216801.ru>
2011-12-21 14:26:29 +0600

    edited bash script

--- killpeak.sh
+++ killpeak.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+param=($@)
 if [ -a $1 ]
 then
     for file in $1*
     do
         echo $file
-        python killerpeak.py $file
+        python killerpeak.py $file ${param[@]:1} # Все эелементы массива, начиная со 2-го
     done
 fi
 echo "Don't forget delete directory rrd_bak if all is ok"
